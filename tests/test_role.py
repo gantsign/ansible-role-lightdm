fb8ea0a2bb99676e20cb2cc4d8416bec23a8458b
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 08:31:32 +0100

    Upgraded to Molecule 1.10.3 (#25)
    Fixed flake8 linter warning.

--- tests/test_role.py
+++ tests/test_role.py
@@ -6,6 +6,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_lightdm_installed(Command):
     assert Command('lightdm --version').rc == 0
 
+
 def test_config_file(File):
     conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
 
@@ -15,6 +16,7 @@ def test_config_file(File):
     assert conf.group == 'root'
     assert oct(conf.mode) == '0644'
 
+
 def test_auto_login(File):
     conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
 
@@ -22,6 +24,7 @@ def test_auto_login(File):
     assert conf.is_file
     assert conf.contains('autologin-user=test_usr')
     assert conf.contains('autologin-user-timeout=4242')
+
 
 def test_guest_login(File):
     conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
