5d1a89733cbf54549552fa7ab52f904595aee12d
John Freeman <john.freeman.opensource@gmail.com>
2018-08-17 13:45:37 +0100

    Changed tests to use host fixture (#58)
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -6,12 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_lightdm_installed(Command):
-    assert Command('lightdm --version').rc == 0
+def test_lightdm_installed(host):
+    assert host.run('lightdm --version').rc == 0
 
 
-def test_config_file(File):
-    conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
+def test_config_file(host):
+    conf = host.file('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
 
     assert conf.exists
     assert conf.is_file
@@ -20,8 +20,8 @@ def test_config_file(File):
     assert oct(conf.mode) == '0644'
 
 
-def test_auto_login(File):
-    conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
+def test_auto_login(host):
+    conf = host.file('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
 
     assert conf.exists
     assert conf.is_file
@@ -29,8 +29,8 @@ def test_auto_login(File):
     assert conf.contains('autologin-user-timeout=4242')
 
 
-def test_guest_login(File):
-    conf = File('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
+def test_guest_login(host):
+    conf = host.file('/usr/share/lightdm/lightdm.conf.d/70-ansible.conf')
 
     assert conf.exists
     assert conf.is_file
